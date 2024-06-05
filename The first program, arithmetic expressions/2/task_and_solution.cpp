81b40c6201e2783bb31ac100704f10cd329772a9
qkue <130838079+qkue@users.noreply.github.com>
2024-06-05 21:21:52 +0300

    Update task_and_solution.cpp

--- The first program, arithmetic expressions/2/task_and_solution.cpp
+++ The first program, arithmetic expressions/2/task_and_solution.cpp
@@ -1,5 +1,19 @@
 /*
+Запустите данный код у себя на компьютере. Какое слово он выведет на экран?
 
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string A = "jqmspo";
+    for (int i = 0; i < A.size(); ++i) {
+        A[i] = (A[i] - i) ^ 25;
+    }
+    cout << A << endl;
+    return 0;
+}
 
 */
 
+sirius
